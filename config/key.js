dda6cff176b8898f26bb63671bd6d14926bb9b0b
Ekkawin <ekkawin.v@gmail.com>
2020-05-18 15:53:35 +0700

    make it better

--- config/key.js
+++ config/key.js
@@ -1,7 +1,7 @@
 // key.js -figure out what set credentials to return
 if (process.env.NODE_ENV == 'production') {
   // we are in the production - return the prod set of keys
-  module.exports = require('./prod');
+  module.exports = require('./prd');
 } else {
   //we are in development - return the dev keys!!
   module.exports = require('./dev');
